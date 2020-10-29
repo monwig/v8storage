469b80cdddb98485b66c1700cf390f2119f0121b
khorevaa <khorevaa@gmail.com>
2020-10-29 13:05:00 +0300

    fix install oscript

--- travis-ci.sh
+++ travis-ci.sh
@@ -2,10 +2,10 @@
 
 set -e
 echo "Устанавливаю версию OScript <$OSCRIPT_VERSION>"
-curl -L https://github.com/oscript-library/ovm/releases/download/v1.0.0-RC15/ovm.exe --output ovm.exe
 
-mono ovm.exe i $OSCRIPT_VERSION
-mono ovm.exe u $OSCRIPT_VERSION
+curl https://oscript.io/downloads/latest/x64/onescript-engine_${OSCRIPT_VERSION}_all.deb > oscript.deb 
+dpkg -i oscript.deb 
+rm -f oscript.deb
 
 opm install 1testrunner; 
 opm install 1bdd; 

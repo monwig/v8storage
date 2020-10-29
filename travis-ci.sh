d9152f8057be3193ed48a098989feb17104e76a4
khorevaa <khorevaa@gmail.com>
2020-10-29 12:43:47 +0300

    fix errors

--- travis-ci.sh
+++ travis-ci.sh
@@ -4,7 +4,8 @@ set -e
 echo "Устанавливаю версию OScript <$OSCRIPT_VERSION>"
 curl -L https://github.com/oscript-library/ovm/releases/download/v1.0.0-RC15/ovm.exe --output ovm.exe
 
-mono ovm.exe $OSCRIPT_VERSION
+mono ovm.exe i $OSCRIPT_VERSION
+mono ovm.exe u $OSCRIPT_VERSION
 
 opm install 1testrunner; 
 opm install 1bdd; 
